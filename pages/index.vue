491cc3146f6cd0fa8fb1b817f7668e2f04482c19
Robert Rypuła <robert.rypula@gmail.com>
2025-03-22 23:45:52 +0100

    First code

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const test = ref<string>('test');
+const counter = ref<number>(0);
+
+const increment = () => {
+    counter.value++;
+}
+</script>
+
+<template>
+    <h1>test {{ test }} - {{ counter }}</h1>
+
+    <button @click="increment()">Increment</button>
+</template>
