d680a58b89aa61c505648743e0c4031ca390a5d2
Robert Rypuła <robert.rypula@gmail.com>
2025-03-23 13:50:31 +0100

    Adds yup validatation library and nav bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,12 +3,15 @@
         <NuxtLink class="link" :class="{ current: $route.path === '/' }" to="/">
             Home
         </NuxtLink>
+        |
         <NuxtLink class="link" :class="{ current: $route.path === '/form/components' }" to="/form/components">
             Form/components
         </NuxtLink>
+        |
         <NuxtLink class="link" :class="{ current: $route.path === '/form/login' }" to="/form/login">
             Form/login
         </NuxtLink>
+        |
         <NuxtLink class="link" :class="{ current: $route.path === '/form/simple' }" to="/form/simple">
             Form/simple
         </NuxtLink>
@@ -20,6 +23,14 @@
 </template>
 
 <style scoped>
+.layout {
+    margin: 32px auto;
+    max-width: 100%;
+    width: 900px;
+    border: 1px solid lightgray;
+    padding: 32px;
+}
+
 .nav .current {
     font-weight: bold;
 }
